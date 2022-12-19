4a834d1b6c66fa3623ef740ac7c9c12904133b70
Anurag <danurag1906@gmail.com>
2022-12-19 21:42:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const navdivElement=document.querySelector('.navdiv');
+const hamElement=document.querySelector('.ham')
+const closeElemet=document.querySelector('.close')
+
+hamElement.addEventListener("click",(e)=>{
+    e.preventDefault()
+    navdivElement.classList.add("active")
+    hamElement.style.display="none"
+    closeElemet.style.display="block"
+})
+
+closeElemet.addEventListener("click",(e)=>{
+    e.preventDefault()
+    navdivElement.classList.remove("active")
+    hamElement.style.display="block"
+    closeElemet.style.display="none"
+})
